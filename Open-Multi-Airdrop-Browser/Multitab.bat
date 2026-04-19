86cf9d5da9046fb7fb84fab3745f63727759dc37
GaoGaoCE <daovuminhhai@outlook.com>
2026-04-19 22:08:29 +0700

    edit dacchain.io

--- Open-Multi-Airdrop-Browser/Multitab.bat
+++ Open-Multi-Airdrop-Browser/Multitab.bat
@@ -11,7 +11,7 @@ start chrome https://www.neftit.xyz/
 start chrome https://hub.konnex.world/points?referral_code=PE0OOO1X
 start chrome https://cedomis.xyz/dashboard
 start chrome https://hub.axisrobotics.ai/?tab=hub
-start chrome https://waitlist.dachain.io/ref/DAC104379
+start chrome https://inception.dachain.io/faucet
 start chrome https://quest.quip.network/airdrop?referral_code=K83J3XBX
 start chrome https://beta.omenx.com/en/events
 start chrome https://testnet.byzanlink.com/vaults/byzanlink-blue-chip-yield-vault?ref=apparent-white-ocelot
